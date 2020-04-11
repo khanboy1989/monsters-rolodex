987e28bd4683c99d8de6900248f98610e69116c0
Serhan Khan <serhan@cocooncreations.net>
2020-04-11 21:51:02 +0300

    cors added

--- src/App.js
+++ src/App.js
@@ -13,9 +13,11 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch('https://jsonplaceholder.typicode.com/users',{mode :'cors'})
     .then(response=> response.json())
-    .then(users=> this.setState({monsters:users}));
+    .then(users=> this.setState({monsters:users}))
+    .catch(ex => console.log(ex))
+    ;
   }
   
   handleChange = e =>{
