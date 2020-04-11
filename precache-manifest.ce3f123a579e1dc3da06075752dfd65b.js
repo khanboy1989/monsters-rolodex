12b54bf0b59d9179b88a21c50699f21e87034083
Serhan Khan <serhan@cocooncreations.net>
2020-04-11 21:14:19 +0300

    Updates

---
+++ precache-manifest.ce3f123a579e1dc3da06075752dfd65b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a1f79ada627976c9260b72c76e836c5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "62e34367000d370f572d",
+    "url": "/monsters-rolodex/static/css/main.d651150e.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62e34367000d370f572d",
+    "url": "/monsters-rolodex/static/js/main.347affcd.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
