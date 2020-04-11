9f1e3516d158de7058ef8ca4b8b3536dfea98ffc
Serhan Khan <serhan@cocooncreations.net>
2020-04-11 21:19:56 +0300

    http to https

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('http://jsonplaceholder.typicode.com/users')
+    fetch('https://jsonplaceholder.typicode.com/users')
     .then(response=> response.json())
     .then(users=> this.setState({monsters:users}));
   }
